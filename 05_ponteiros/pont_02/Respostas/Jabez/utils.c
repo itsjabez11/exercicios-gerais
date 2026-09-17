5cc898d1e272d47f189ed5f5a38f9a65d273da6a
itsjabez11 <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>
2026-09-17 14:50:12 -0300

    pont_02

---
+++ 05_ponteiros/pont_02/Respostas/Jabez/utils.c
@@ -0,0 +1,88 @@
+#include "utils.h"
+#include <stdio.h>
+
+/**
+ * @brief Lê do usuário um intervalo de números inteiros.
+ * 
+ * Esta função recebe como parâmetro dois ponteiros para inteiros e lê do usuário os valores correspondentes ao início e fim de um intervalo.
+ * 
+ * @param m Ponteiro para a variável que receberá o valor do início do intervalo.
+ * @param n Ponteiro para a variável que receberá o valor do fim do intervalo.
+ */
+void LeIntervalo(int * m, int * n){
+
+    int a,b;
+
+    scanf("%d %d ",&a,&b);
+
+    *m = a;
+    *n = b;
+
+}
+
+/**
+ * @brief Verifica se um número é primo.
+ * 
+ * Esta função recebe como parâmetro um número inteiro e verifica se ele é primo.
+ * 
+ * @param n Número a ser verificado.
+ * @return 1 se o número é primo, 0 caso contrário.
+ */
+int EhPrimo(int n){
+
+    int c = 0;
+
+    for(int i = n; i>0; i--){
+        
+        if(n%i==0){
+            c++;
+        }
+    }
+
+    if(c==2){
+        return 1;
+    }
+
+    return 0;
+}
+
+/**99999
+ * @brief Obtém o maior e o menor número primo em um intervalo.
+ * 
+ * Esta função recebe como parâmetro dois números inteiros que representam o início e o fim de um intervalo, e dois ponteiros para inteiros que receberão os valores do maior e do menor número primo encontrado no intervalo.
+ * 
+ * @param m Início do intervalo.
+ * @param n Fim do intervalo.
+ * @param menor Ponteiro para a variável que receberá o menor número primo encontrado.
+ * @param maior Ponteiro para a variável que receberá o maior número primo encontrado.
+ */
+void ObtemMaiorEMenorPrimo(int m, int n, int *menor, int *maior){
+
+
+    int menor1 = 999999, maior1 =-999999;
+
+    int jaTem = 0, jaTem2 = 0;
+
+    for(int i = m; i<=n ; i++){
+
+        if(EhPrimo(i)){
+
+            if(i<menor1 && jaTem==0){
+
+                *menor = i;
+                jaTem = 1;
+
+            }
+
+            if(i>maior1 && jaTem2==0){
+
+                *maior = i;
+
+            }
+        }
+        
+    }
+
+
+}
+
